e07a641342e19f2f15719c3e1651f956515b54a9
Soheil Ghanbary <soli.ghanbary@gmail.com>
2024-12-09 22:28:31 +0330

    add hero image to local

--- src/components/features/Hero.tsx
+++ src/components/features/Hero.tsx
@@ -7,8 +7,8 @@ export default function Hero() {
     <div className="relative flex flex-col items-center justify-center gap-4 px-4 py-20 lg:h-[600px] lg:py-0">
       <div className="-z-10 absolute size-full bg-black/65" />
       <img
-        alt="hero"
-        src="https://images.unsplash.com/photo-1441984904996-e0b6ba687e04?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="Shopino"
+        src="/hero.jpg"
         className="-z-20 absolute size-full object-cover"
       />
       <h1 className="text-center font-black text-2xl text-white lg:text-5xl">
