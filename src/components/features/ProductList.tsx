b2d0bfb0df90411a3f860eb28e198009467aca2b
Soheil Ghanbary <soli.ghanbary@gmail.com>
2024-12-09 22:24:32 +0330

    update product loader in products page

--- src/components/features/ProductList.tsx
+++ src/components/features/ProductList.tsx
@@ -4,19 +4,8 @@ import { ShoppingCartIcon } from 'lucide-react'
 import Link from 'next/link'
 import { Fragment, useEffect, useRef } from 'react'
 import { Button } from '../ui/button'
-import { Skeleton } from '../ui/skeleton'
+import ProductSkeleton from './ProductSkeleton'
 import ProductImage from './product/ProductImage'
-
-const ProductSkeleton = () => {
-  return Array.from({ length: 12 }).map((_, i) => (
-    <div key={i} className="flex flex-col gap-2">
-      <Skeleton className="aspect-square rounded-lg" />
-      <Skeleton className="h-5 w-2/3" />
-      <Skeleton className="h-5 w-1/3" />
-      <Skeleton className="h-9 w-full" />
-    </div>
-  ))
-}
 
 export default () => {
   const {
@@ -91,8 +80,8 @@ export default () => {
             </Fragment>
           ))
         )}
+        {isFetchingNextPage && <ProductSkeleton />}
       </div>
-      {isFetchingNextPage && <p>Loading more...</p>}
       <div ref={observerRef} style={{ height: '1px' }} />
     </div>
   )
