fac3913868243d4994107e80a9c202787e16f5f8
Soheil Ghanbary <soli.ghanbary@gmail.com>
2024-12-10 14:22:28 +0330

    update caching

--- src/services/product.ts
+++ src/services/product.ts
@@ -2,7 +2,7 @@ export async function getProducts(offset = 0, limit = 10) {
   const res = await fetch(
     `https://api.escuelajs.co/api/v1/products?offset=${offset}&limit=${limit}`,
     {
-      cache: 'no-store',
+      cache: 'force-cache',
     }
   )
   if (!res.ok) throw new Error('Failed to fetch products')
@@ -10,6 +10,8 @@ export async function getProducts(offset = 0, limit = 10) {
 }
 
 export async function getProduct(id: string) {
-  const res = await fetch(`https://api.escuelajs.co/api/v1/products/${id}`)
+  const res = await fetch(`https://api.escuelajs.co/api/v1/products/${id}`, {
+    cache: 'reload',
+  })
   return (await res.json()) as Product
 }
